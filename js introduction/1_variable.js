ae92ada52b5e600a5279dc6eb7f9e62a47b23138
aniket265528 <aniket265528@gmail.com>
2022-01-21 04:27:00 +0530

    sateesh

---
+++ js introduction/1_variable.js
@@ -0,0 +1,63 @@
+//variable declaration
+
+//var , let , const
+
+/* //var flag=true
+var num=13
+
+for(var i=2;i*i<num;i++){
+    if(num%i==0)
+    {
+        flag=false
+        break
+    }
+}
+
+if(flag==true){
+    console.log("prime number")
+}
+else {
+    console.log("not prime number")
+}*/
+/*var b=2
+var n=2.3
+var f=2.4556
+console.log(b)
+console.log(n)
+console.log(f)*/
+
+
+//function
+
+/*function add(a,b){
+    console.log(a+b)
+
+}
+
+add(5,6)
+
+function sub(a,b){
+    console.log(a-b)
+}
+sub(9,4)
+
+function mul(a,b){
+    console.log(a*b)
+}
+
+mul(3*5)
+
+function div(a,b)
+{
+    console.log(a%b)
+
+}
+
+div(4,2)*/
+
+let multi=(function(a,b){
+console.log(a*b)
+})(2,5)
+
+
+
